9cb2e6793f9e3d6378c219cf290eb01050927a12
dchapman <david@ronikdesign.com>
2019-07-29 19:19:47 -0400

    non-working implementation of simple instafeed import of Amy's photos; adding new .graphqlconfig and tsg-project.graphql build files

--- src/javascripts/components/instagram.js
+++ src/javascripts/components/instagram.js
@@ -1,11 +1,21 @@
 import Imageloaded from '../directives/imageloaded';
+import {Instafeed} from 'instafeed';
 
 export default {
   name: 'instagram',
   data() {
     return {};
   },
-  mounted() {},
+  mounted() {
+    const amyFeed = new Instafeed({
+      get: 'user',
+      userId: '1050599',
+      accessToken: '1050599.1677ed0.1704518e6a6047718b118a2d6d7fd08c',
+      target: '.instafeed-row'
+    });
+
+    amyFeed.run();
+  },
   methods: {},
   directives: {
     imageloaded: Imageloaded
